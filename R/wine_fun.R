1635debbc754f715be30d97d3ec9ab65c1ebd9f4
Anne Bozack <anne.bozack@gmail.com>
2017-04-24 00:36:56 -0400

    Create wine_fun.R

---
+++ R/wine_fun.R
@@ -0,0 +1,24 @@
+#' Plot function
+#'
+#' This function generates a plot with linear or exponential growth
+#' @param type Linear or exponential? Default is type = 'linear' (options are type='linear' or type='exponential')
+#' @param growth How much does wine effect happiness? Default is growth = 1
+#' @keywords wine
+#' @export
+#' @examples alexBday_fun(type='exponential', growth='5')
+#' alexBday()
+
+alexBday_fun = function(type='linear', growth=1){
+	funLinear = function(x){
+		x*growth
+	}
+	funExp = function(x){
+		x^growth
+	}
+	p <- ggplot2::ggplot(data = data.frame(x = 0), mapping = aes(x = x))
+	if (type == 'linear'){
+		p + stat_function(fun = funLinear) + xlim(0,100) + labs(x='Wine (glasses)', y='Happiness (N smiles)', title='Relationship between wine consumption and happiness')
+	} else if (type == 'exponential'){
+		p + stat_function(fun = funExp) + xlim(0,100)
+	}
+}
